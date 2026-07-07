b6d4ee4cfaf324221f5c9e2264c0cccd82afb213
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-07-07 20:16:43 +0530

    Sum and Difference of 2 numbers in HTML

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function calculator(){
+    let n1=8;
+    let n2=4;
+    let sum1=n1+n2;
+    let diff1=n1-n2;
+    document.getElementById("showSum").textContent=sum1;
+    document.getElementById("showDiff").textContent=diff1;
+}
+calculator();
